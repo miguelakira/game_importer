29bf3b58084a313ec9d8c970831f323ab4e7e570
Miguel Akira <miguel.akira@gmail.com>
2013-03-19 16:00:39 -0300

    ajustes no layout e exibicao de imagens

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :console_id, :description, :discount, :name, :price, :quantity, :cost, :sales
+  attr_accessible :console_id, :description, :discount, :name, :price, :quantity, :cost, :sales, :image_url
   has_paper_trail
   belongs_to :console
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318203511) do
+ActiveRecord::Schema.define(:version => 20130319184107) do
 
   create_table "carts", :force => true do |t|
     t.integer  "user_id"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20130318203511) do
     t.datetime "updated_at",                                :null => false
     t.decimal  "cost",        :precision => 8, :scale => 2
     t.integer  "sales"
+    t.string   "image_url"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
